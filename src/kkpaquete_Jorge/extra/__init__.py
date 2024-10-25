3f80ae6d8121c5668457d58abcb31f6148c6d20d
calera-dlsi <calera@dlsi.ua.es>
2024-10-25 11:19:23 +0200

    Subido el paquete

---
+++ src/kkpaquete_Jorge/extra/__init__.py
@@ -0,0 +1,3 @@
+from .otros import multiplica, divide, pi
+
+
---
+++ tests/main.py
@@ -0,0 +1,9 @@
+import kkpaquete_Jorge as kp
+import kkpaquete_Jorge.extra as kpr
+from math import sin
+
+print(kp.suma(5,3))
+print(kp.resta(5,3))
+print(kpr.multiplica(5,3))
+print(kpr.divide(5,3))
+print(sin(kpr.pi/2))
